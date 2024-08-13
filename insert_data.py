55aa71e7d293c8d42c973fde5211ff98378ae43b
kkumyk <k.kumykova@gmail.com>
2024-08-13 11:58:22 +0100

    check if an input file contains at least three lines; if no translation is found return message; adjust script to process multiple files

--- insert_data.py
+++ insert_data.py
@@ -5,17 +5,14 @@ import psycopg2
 import requests
 from lxml import html
 from bs4 import BeautifulSoup
-import pandas as pd
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 
-# will search for .env file in local folder and load variables
+# will search for .env file in local folder and load variables from the .env file
 load_dotenv() 
 
 # wait for the database to be ready
 time.sleep(10)
 
+# database connection params
 conn_params = {
     'dbname':os.getenv('DB_NAME'), 
     'user':os.getenv('DB_USER'),
@@ -67,13 +64,13 @@ try:
     conn.commit()
     
     
-    poem_file_path = 'poems/poem.txt'
-    poem_id = None # the ID of the poem being processed
-    
     def insert_poem(poem_file):
 
         with open(poem_file, 'r') as file:
             lines = file.readlines()
+            
+        if len(lines) < 3:
+            raise ValueError(f"File {poem_file} does not have enough lines to process. Skipping.")
 
         poem_contents = {"author":"", "title": "", "contents":""}
         poem_contents["author"] = lines[0].rstrip() # extract the author from the poem.txt file
@@ -91,31 +88,6 @@ try:
         
         return poem_to_insert
     
-    insert_poem_query = '''
-        INSERT INTO poems_poem (title, author, contents)
-        VALUES  (%s,%s,%s)
-        ON CONFLICT (title, author) DO NOTHING
-        RETURNING id;
-    '''
-    cur.execute(insert_poem_query, insert_poem(poem_file_path))
-    
-    # check if cur.fetchone() returns a result   
-    result = cur.fetchone()
-    if result:
-        poem_id = result[0]
-    else:
-        raise Exception("No new poem was inserted, it might already exist in the database. Execution stopped.")
-
-    
-    conn.commit()
-    
-    
-    # fetch and print the data
-    cur.execute('SELECT * FROM poems_poem;')
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-        
     
     def process_text_file(file_path):
         with open(file_path, 'r') as file:
@@ -134,7 +106,6 @@ try:
         word_urls = ["https://kbd.wiktionary.org/wiki/" + word for word in unique_words]
         return word_urls
     
-    # print(process_text_file(poem_file_path))
     
     def check_urls_and_extract_word_meta(urls):
         results = []
@@ -147,8 +118,16 @@ try:
                     
                     if len(h1_tag.get_text()) > 2:
                         tree = html.fromstring(response.content)
-                        eng_translation = tree.xpath('//h5[.="ИНДЖЫЛЫБЗЭ"]/following::ul[1]/li/text()')[0].split(":")[1].strip()
-                        rus_translation = tree.xpath('//h5[.="УРЫСЫБЗЭ"]/following::ul[1]/li/text()')[0].split(":")[1].strip()
+                        
+                        try:
+                            eng_translation = tree.xpath('//h5[.="ИНДЖЫЛЫБЗЭ"]/following::ul[1]/li/text()')[0].split(":")[1].strip()
+                        except IndexError:
+                            eng_translation = "Translation not found"
+
+                        try:
+                            rus_translation = tree.xpath('//h5[.="УРЫСЫБЗЭ"]/following::ul[1]/li/text()')[0].split(":")[1].strip()
+                        except IndexError:
+                            rus_translation = "Translation not found"                       
                         
                         print(eng_translation, rus_translation)
                         results.append({
@@ -158,7 +137,8 @@ try:
                         })
                         
             except requests.RequestException as e:
-                results.append({url: f"Request failed: {e}"})
+                print(f"Request failed for URL {url}: {e}")
+                continue
         return results
 
 
@@ -168,7 +148,7 @@ try:
         # array of objects with word, eng and rus translations
         extracted_contents = check_urls_and_extract_word_meta(word_urls)
         print(extracted_contents)
-        
+
         words_to_insert = []
         
         '''
@@ -196,29 +176,59 @@ try:
                 word_id = cur.fetchone()[0]
             
             word_to_id_map[word] = word_id
-            
             words_to_insert.append((word, eng_transl, rus_transl))
-        print(words_to_insert)
         
-        # insert into poems_poem_words table
         poems_poem_words_to_insert = [(poem_id, word_to_id_map[word_obj["word"]]) for word_obj in extracted_contents]
-        
         return words_to_insert, poems_poem_words_to_insert
     
     
-    words_to_insert, poems_poem_words_to_insert = add_new_words_to_word_pool(poem_file_path, poem_id)
+    # get all txt file in the poems dir
+    poems_dir = 'poems/'
+    poem_files =  ['poems/'+f for f in os.listdir(poems_dir) if f.endswith('.txt')]
         
-    insert_words_query = 'INSERT INTO poems_word (word, eng_transl, rus_transl) VALUES (%s,%s,%s) ON CONFLICT (word) DO NOTHING;'
-    cur.executemany(insert_words_query, words_to_insert)
-    conn.commit()
+    for poem_file_path in poem_files:
     
-    insert_poems_poem_words_query = 'INSERT INTO poems_poem_words (poem_id, word_id) VALUES (%s, %s) ON CONFLICT DO NOTHING'
-    cur.executemany(insert_poems_poem_words_query, poems_poem_words_to_insert)
-    conn.commit()
-    
+        try:
+            poem_to_insert = insert_poem(poem_file_path)
+
+            insert_poem_query = '''
+                INSERT INTO poems_poem (title, author, contents)
+                VALUES  (%s,%s,%s)
+                ON CONFLICT (title, author) DO NOTHING
+                RETURNING id;
+            '''
+            
+            # insert the poem and retrieve its ID
+            cur.execute(insert_poem_query, poem_to_insert)
+            result = cur.fetchone()
+
+            if result:
+                poem_id = result[0]
+                words_to_insert, poems_poem_words_to_insert = add_new_words_to_word_pool(poem_file_path, poem_id)
+
+                insert_words_query = 'INSERT INTO poems_word (word, eng_transl, rus_transl) VALUES (%s,%s,%s) ON CONFLICT (word) DO NOTHING;'
+                cur.executemany(insert_words_query, words_to_insert)
+                conn.commit()
+
+                insert_poems_poem_words_query = 'INSERT INTO poems_poem_words (poem_id, word_id) VALUES (%s, %s) ON CONFLICT DO NOTHING'
+                cur.executemany(insert_poems_poem_words_query, poems_poem_words_to_insert)
+                conn.commit()
+
+            else:
+                print(f"No new poem was inserted for {poem_file_path}, it might already exist in the database.")
+
+        except ValueError as ve:
+            print(ve)
+        except Exception as e:
+            print(f"An error occurred while processing {poem_file_path}: {e}")
+
+    cur.execute('SELECT * FROM poems_poem;')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
     cur.close()
     conn.close()
-        
 
 except Exception as e:
     print(f"An error occurred: {e}")
@@ -227,3 +237,4 @@ finally:
         cur.close()
     if conn:
         conn.close()
+    
